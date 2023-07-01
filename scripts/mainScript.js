10fdce76c5d56bf3f25e919ab54e2d0be39c643a
r3 <122603849+r3workz@users.noreply.github.com>
2023-07-01 22:59:38 +0530

    Initial commit

---
+++ scripts/mainScript.js
@@ -0,0 +1,6 @@
+const aside = document.querySelector("#sideBar");
+const toggleAside = document.querySelector(".toggleAside");
+
+
+
+console.log('Had no time to finish. submitting just for the Contest attempt, To be eligible for Module Test')
