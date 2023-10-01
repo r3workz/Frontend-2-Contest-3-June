a40eca9cdbdf265ff0e56d786049a1936d6adee0
r3workz <122603849+r3workz@users.noreply.github.com>
2023-10-01 22:45:31 +0530

    fix

--- scripts/common.js
+++ scripts/common.js
@@ -1,69 +1,83 @@
-const BASE_URL = "https://www.googleapis.com/youtube/v3";
-const API_KEY = "AIzaSyArFYYF3huX2wKXpL_vUsDUK5rz_I8wKLY";
+const BASE_URL = "https://www.googleapis.com/youtube/v3"
+const API_KEY = "AIzaSyArFYYF3huX2wKXpL_vUsDUK5rz_I8wKLY"
 
-const createVideoItem = (item) => {
-    // console.log(item);
-    const vidItem = document.createElement("div");
-    vidItem.className = "video-item";
-    vidItem.innerHTML = `
+const createVideoItem = item => {
+	// console.log(item);
+	const vidItem = document.createElement("div")
+	vidItem.className = "video-item"
+	vidItem.innerHTML = `
     <img class="vi-image" src="${item.snippet.thumbnails.medium.url}" alt=""/>
     <div class="vi-title"><p>${item.snippet.title}</p></div>
     <div class="vi-channel">${item.snippet.channelTitle}</div>
-    <div class="vi-views-uploaded">${convertCount(
-        item.statistics.viewCount
-    )} views . ${convertDateToRelativeTime(item.snippet.publishedAt)}</div>
-    `;
-    vidItem.addEventListener("click", () => {
-        sessionStorage.setItem("video-id", item.id);
-        window.location.pathname = "/videoDetails.html";
-    });
-    return vidItem;
-};
+    
+    `
+	// <div class="vi-views-uploaded">${convertCount(
+	// 	item.statistics.viewCount
+	// )} views . ${convertDateToRelativeTime(item.snippet.publishedAt)}</div>
+
+	const statistics = document.createElement("div")
+	statistics.className = "vi-views-uploaded"
+
+	if (item.statistics) {
+		statistics.innerHTML = `
+        ${convertCount(
+			item.statistics.viewCount
+		)} views . ${convertDateToRelativeTime(item.snippet.publishedAt)}
+        `
+	}
+	vidItem.appendChild(statistics)
+
+	vidItem.addEventListener("click", () => {
+		sessionStorage.setItem("video-id", item.id)
+		window.location.pathname = "/videoDetails.html"
+	})
+	return vidItem
+}
 
 // Convert date to relative time (1 day ago, 2 hours ago, etc)
 function convertDateToRelativeTime(dateString) {
-    const date = new Date(dateString);
-    const now = new Date();
-    const difference = now - date;
+	const date = new Date(dateString)
+	const now = new Date()
+	const difference = now - date
 
-    const seconds = Math.floor(difference / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const weeks = Math.floor(days / 7);
+	const seconds = Math.floor(difference / 1000)
+	const minutes = Math.floor(seconds / 60)
+	const hours = Math.floor(minutes / 60)
+	const days = Math.floor(hours / 24)
+	const weeks = Math.floor(days / 7)
 
-    let unit;
-    let number;
+	let unit
+	let number
 
-    if (weeks > 0) {
-        unit = weeks > 1 ? "weeks" : "week";
-        number = weeks;
-    } else if (days > 0) {
-        unit = days > 1 ? "days" : "day";
-        number = days;
-    } else if (hours > 0) {
-        unit = hours > 1 ? "hours" : "hour";
-        number = hours;
-    } else if (minutes > 0) {
-        unit = minutes > 1 ? "minutes" : "minute";
-        number = minutes;
-    } else {
-        unit = "seconds";
-        number = seconds;
-    }
+	if (weeks > 0) {
+		unit = weeks > 1 ? "weeks" : "week"
+		number = weeks
+	} else if (days > 0) {
+		unit = days > 1 ? "days" : "day"
+		number = days
+	} else if (hours > 0) {
+		unit = hours > 1 ? "hours" : "hour"
+		number = hours
+	} else if (minutes > 0) {
+		unit = minutes > 1 ? "minutes" : "minute"
+		number = minutes
+	} else {
+		unit = "seconds"
+		number = seconds
+	}
 
-    return `${number} ${unit} ago`;
+	return `${number} ${unit} ago`
 }
 
 // Convert viewCount to human readable(K=1000, M=1000000)
 function convertCount(viewCount) {
-    const K = 1000;
-    const M = 1000000;
-    if (viewCount < K) {
-        return `${viewCount}`;
-    }
-    if (viewCount < M) {
-        return `${(viewCount / K).toFixed(1)} K`;
-    }
-    return `${(viewCount / M).toFixed(1)} M`;
+	const K = 1000
+	const M = 1000000
+	if (viewCount < K) {
+		return `${viewCount}`
+	}
+	if (viewCount < M) {
+		return `${(viewCount / K).toFixed(1)} K`
+	}
+	return `${(viewCount / M).toFixed(1)} M`
 }
--- scripts/home.js
+++ scripts/home.js
@@ -1,98 +1,105 @@
-const homeUrl = `${BASE_URL}/videos?part=snippet,statistics&chart=mostPopular&regionCode=IN&maxResults=20&key=${API_KEY}`;
+const homeUrl = `${BASE_URL}/videos?part=snippet,statistics&chart=mostPopular&regionCode=IN&maxResults=20&key=${API_KEY}`
 
-let cache;
+let cache
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const aside = document.querySelector("#sideBar");
-    const toggleAside = document.querySelector(".toggleAside");
-    const videoGrid = document.getElementById("video-grid");
+	const aside = document.querySelector("#sideBar")
+	const toggleAside = document.querySelector(".toggleAside")
+	const videoGrid = document.getElementById("video-grid")
 
-    const searchBox = document.getElementById("searchInput");
-    const searchBtn = document.getElementById("searchButton");
-    const filters = document.getElementById("filters");
+	const searchBox = document.getElementById("searchInput")
+	const searchBtn = document.getElementById("searchButton")
+	const filters = document.getElementById("filters")
 
-    const scrollRight = document.getElementById("scroll-right");
+	const scrollRight = document.getElementById("scroll-right")
 
-    scrollRight.addEventListener("click", () => {
-        scrollRight.parentElement.scrollLeft += 300;
-    });
+	scrollRight.addEventListener("click", () => {
+		scrollRight.parentElement.scrollLeft += 300
+	})
 
-    // ! Show/Hide SideBar
-    toggleAside.addEventListener("click", () => {
-        aside.classList.toggle("hiddenElement");
-    });
+	// ! Show/Hide SideBar
+	toggleAside.addEventListener("click", () => {
+		aside.classList.toggle("hiddenElement")
+	})
 
-    // ! Search Videos
-    searchBtn.addEventListener("click", () => {
-        const searchText = searchBox.value.trim().replaceAll(" ", "+");
-        videoGrid.innerHTML = "";
-        searchVideo(searchText, videoGrid);
-    });
+	// ! Search Videos
+	searchBtn.addEventListener("click", () => {
+		const searchText = searchBox.value.trim().replaceAll(" ", "+")
+		videoGrid.innerHTML = ""
+		searchVideo(searchText, videoGrid)
+	})
+	searchBox.addEventListener("keyup", e => {
+		if (e.key === "Enter") {
+			const searchText = searchBox.value.trim().replaceAll(" ", "+")
+			videoGrid.innerHTML = ""
+			searchVideo(searchText, videoGrid)
+		}
+	})
 
-    cache = await caches.open("videos-cache");
+	cache = await caches.open("videos-cache")
 
-    const cachedItems = await cache.match("video-items");
+	const cachedItems = await cache.match("video-items")
 
-    if (cachedItems) {
-        try {
-            const cachedItemsData = await cachedItems.json();
-            cachedItemsData.items.map((item) => {
-                videoGrid.appendChild(createVideoItem(item));
-            });
-        } catch (error) {
-            console.error("Error parsing cached items:", error);
-        }
-    } else {
-        try {
-            const response = await fetch(homeUrl);
-            const responseData = await response.json();
-            if (responseData.error) {
-                console.error(responseData.error.message);
-            } else {
-                // Store in cache
-                cache.put(
-                    "video-items",
-                    new Response(JSON.stringify(responseData))
-                );
-                // Render Data in Page
-                responseData.items.map((item) => {
-                    videoGrid.appendChild(createVideoItem(item));
-                });
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    fetchCategories().then((categories) => {
-        // console.log(categories);
-        categories.map((category) => {
-            const filterItem = document.createElement("div");
-            filterItem.className = "filter-item";
-            filterItem.innerHTML = `<p>${category.snippet.title}</p>`;
-            filters.appendChild(filterItem);
-        });
-    });
-});
+	if (cachedItems) {
+		try {
+			const cachedItemsData = await cachedItems.json()
+			cachedItemsData.items.map(item => {
+				videoGrid.appendChild(createVideoItem(item))
+			})
+		} catch (error) {
+			console.error("Error parsing cached items:", error)
+		}
+	} else {
+		try {
+			const response = await fetch(homeUrl)
+			const responseData = await response.json()
+			if (responseData.error) {
+				console.error(responseData.error.message)
+			} else {
+				// Store in cache
+				cache.put(
+					"video-items",
+					new Response(JSON.stringify(responseData))
+				)
+				// Render Data in Page
+				responseData.items.map(item => {
+					videoGrid.appendChild(createVideoItem(item))
+				})
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	fetchCategories().then(categories => {
+		// console.log(categories);
+		categories.map(category => {
+			const filterItem = document.createElement("div")
+			filterItem.className = "filter-item"
+			filterItem.innerHTML = `<p>${category.snippet.title}</p>`
+			filters.appendChild(filterItem)
+		})
+	})
+})
 const searchVideo = (searchText, videoGrid) => {
-    fetch(
-        `${BASE_URL}/search?part=snippet,statistics&q=${searchText}&maxResults=20&key=${API_KEY}`
-    )
-        .then(async (response) => await response.json())
-        .then((data) => {
-            if (data.error) {
-                console.error(data.error.message);
-            } else {
-                data.items.map((item) => {
-                    videoGrid.appendChild(createVideoItem(item));
-                });
-            }
-        });
-};
+	fetch(
+		`${BASE_URL}/search?part=snippet&q=${searchText}&maxResults=20&key=${API_KEY}`
+	)
+		.then(async response => await response.json())
+		.then(data => {
+			if (data.error) {
+				console.error(data.error.message)
+			} else {
+				data.items.map(item => {
+					videoGrid.appendChild(createVideoItem(item))
+				})
+			}
+		})
+}
 
 const fetchCategories = async () => {
-    const response = await fetch(
-        `${BASE_URL}/videoCategories?part=snippet&regionCode=IN&key=${API_KEY}`
-    );
-    const json = await response.json();
-    return json.items;
-};
+	const response = await fetch(
+		`${BASE_URL}/videoCategories?part=snippet&regionCode=IN&key=${API_KEY}`
+	)
+	const json = await response.json()
+	return json.items
+}
